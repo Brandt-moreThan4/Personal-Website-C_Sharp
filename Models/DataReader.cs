6a03e09a5b03388784c423d46519502d1c63eb6e
Brandt Green <153144Green@chsbr.net>
2020-04-17 19:32:00 -0500

    Put base structure and temporary content to play with styling

---
+++ Models/DataReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web;
+
+namespace FunWithBrandt.Models
+{
+    public class DataReader
+    {
+        public static List<KnowledgeRecord> KnowledgeRead()
+        {
+
+            var records = new List<KnowledgeRecord>();
+            //var path = @"C:\Users\bgreen3\Desktop\PersonalWebsite\wwwroot\Data\KnowledgeRepoTable.csv";
+            var path = Directory.GetCurrentDirectory() + @"\wwwroot\Data\KnowledgeRepoTable.csv";
+            var line = string.Empty; string [] splitString;
+            KnowledgeRecord record;
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sw = new StreamReader(path))
+                {
+                    line = sw.ReadLine();
+                    while (line != null)
+                    {
+                        splitString = line.Split(",");
+                        record = new KnowledgeRecord();                        
+                        record.Person = splitString[0];
+                        record.Institution = splitString[1];
+                        record.Description = splitString[2];
+                        record.Source = splitString[3];
+                        record.Keywords = splitString[4];
+                        records.Add(record);
+                        line = sw.ReadLine();
+                    }                    
+                }
+            }
+
+            return records;
+
+        }
+    }
+    
+}
--- Pages/KnowledgeRepository.cshtml.cs
+++ Pages/KnowledgeRepository.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FunWithBrandt.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,12 @@ namespace FunWithBrandt
 {
     public class KnowledgeRepositoryModel : PageModel
     {
+        public List<KnowledgeRecord> knowledgeRecords;
+
+
         public void OnGet()
         {
-
+            knowledgeRecords = DataReader.KnowledgeRead();
         }
     }
 }
