b713a75ce9ffeca17259a24049dc067080b8318b
Brandt Green <153144Green@chsbr.net>
2020-04-18 10:24:31 -0500

    HTML for both Home page and program page.

--- Models/ProgramPost.cs
+++ Models/ProgramPost.cs
@@ -7,11 +7,12 @@ namespace FunWithBrandt.Models
 {
     public class ProgramPost
     {
-        public string Language { get; set; }
+        public string Code { get; set; }
         public string Description { get; set; }
+        public string Keywords { get; set; }
+        public string Language { get; set; }
         public string Source { get; set; }
         public string Title { get; set; }
-        public string Keywords { get; set; }
 
     }
 }
