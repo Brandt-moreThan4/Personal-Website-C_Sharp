3ed134dfcb0a11dd6629ac9392c2026557570ca7
Brandt Green <153144Green@chsbr.net>
2020-04-18 09:39:00 -0500

    Filler Text for book and graph pages.

--- Models/ProgramPost.cs
+++ Models/ProgramPost.cs
@@ -7,6 +7,11 @@ namespace FunWithBrandt.Models
 {
     public class ProgramPost
     {
+        public string Language { get; set; }
+        public string Description { get; set; }
+        public string Source { get; set; }
+        public string Title { get; set; }
+        public string Keywords { get; set; }
 
     }
 }
