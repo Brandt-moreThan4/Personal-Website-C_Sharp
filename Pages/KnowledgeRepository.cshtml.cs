7ccb7814bc81d01ce224e702e07666dd49fa8e60
Brandt Green <153144Green@chsbr.net>
2020-04-23 22:31:23 -0500

    Refined search and added a shit ton of images

--- Pages/KnowledgeRepository.cshtml.cs
+++ Pages/KnowledgeRepository.cshtml.cs
@@ -11,18 +11,47 @@ namespace FunWithBrandt
     public class KnowledgeRepositoryModel : PageModel
     {
         public List<KnowledgeRecord> knowledgeRecords;
+        public List<KnowledgeRecord> displayedKnowledgeRecords;
         public bool ShowHubbel;
+
+        [BindProperty(SupportsGet = true) ]
+        public string SearchTerm { get; set; }
+
+        public KnowledgeRepositoryModel()
+        {
+            knowledgeRecords = DataReader.KnowledgeRead();
+        }
 
         public void OnGet()
         {
-            knowledgeRecords = DataReader.KnowledgeRead();
+            displayedKnowledgeRecords = GetKnowledgeRecordsByKeyWord();
         }
 
         public  IActionResult OnPost()
         {
             ShowHubbel = true;
-            knowledgeRecords = DataReader.KnowledgeRead();
+            displayedKnowledgeRecords = GetKnowledgeRecordsByKeyWord();
             return Page();
+        }
+
+        public List<KnowledgeRecord> GetKnowledgeRecordsByKeyWord()
+        {
+            var searchString = this.SearchTerm;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.ToUpper();
+            }
+          
+            var  records = from k in knowledgeRecords
+                           where string.IsNullOrEmpty(searchString) || k.Keywords.ToUpper().Contains(searchString) ||
+                           k.Description.ToUpper().Contains(searchString) || k.Person.ToUpper().Contains(searchString)
+                           
+                           orderby k.Person
+                           select k;
+
+            return records.ToList();
+
         }
 
     }
